d91ac33923cbff4c403e7f083dfed033e82cd210
Nathan Rymarz <nrymarz@gmail.com>
2020-10-18 22:52:13 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,20 @@ const codes = [
   "b",
   "a"
 ];
-
+function codecheck(e) {
+  let index = 0;
+  const key = e.key;
+  if (key === codes[index]) {
+    index++;
+    if (index === codes.length) {
+      alert("Congratulations!");
+    }
+  }
+  else {
+    index = 0;
+  }
+}
+}
 function init() {
-  // your code here
+  document.body.addEventListener("keydown",codecheck(e))
 }
